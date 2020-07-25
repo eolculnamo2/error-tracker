64faea498075d7ae27db84c4b602e1f8c5dcbfb6
Rob Bertram <robert.bertram@livingasone.com>
2020-07-25 01:13:02 -0500

    add js bundle to include on site

---
+++ js-tracker/src/index.ts
@@ -0,0 +1,41 @@
+const ERROR_PROCESSOR_URL = "http://localhost:8088";
+
+(() => {
+  function sendError(
+    msg: string | Event,
+    url: string | undefined,
+    lineNo: number | undefined,
+    columnNo: number | undefined,
+    err: Error | undefined
+  ) {
+    fetch(ERROR_PROCESSOR_URL + "/receive-error", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        msg,
+        url,
+        lineNo,
+        columnNo,
+        err,
+      }),
+    });
+  }
+
+  window.onerror = function (
+    msg: string | Event,
+    url: string | undefined,
+    lineNo: number | undefined,
+    columnNo: number | undefined,
+    err: Error | undefined
+  ): boolean {
+    try {
+      sendError(msg, url, lineNo, columnNo, err);
+    } catch (e) {
+      console.log(e);
+    }
+    return false;
+  };
+})();

827563e28fe4874ad3a6d98d572a1aee5a564004
Brian <briansterling93@yahoo.com>
2020-07-29 01:42:32 -0600

    dashboard work

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import {
+  MainSection,
+  SecondarySection,
+  SideBar,
+} from "../styling/DashboardStyling";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Navbar />
+      <MainSection>
+        <SideBar></SideBar>
+      </MainSection>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ client/src/styling/DashboardStyling.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MainSection = styled.div`
+  align-items: center;
+`;
+
+export const SecondarySection = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const SideBar = styled.div`
+  margin-top: 2rem;
+  width: 15rem;
+  height: 35rem;
+  border 1px solid #999;
+
+  border-radius: 0.7rem;
+`;
